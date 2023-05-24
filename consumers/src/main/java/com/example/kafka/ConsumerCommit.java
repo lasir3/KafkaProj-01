376c7fd24193bd43a938d9504851529ad5a6ea3f
edk2613 <lasir3@gmail.com>
2023-05-24 14:36:42 +0900

    Auto Commit Test

---
+++ consumers/src/main/java/com/example/kafka/ConsumerCommit.java
@@ -0,0 +1,86 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerCommit {
+    public static final Logger logger = LoggerFactory.getLogger(ConsumerCommit.class.getName());
+
+    public static void main(String[] args) {
+
+        String topicName = "pizza-topic";
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.73:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+        // props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-03");
+        props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "5000");
+
+
+        // poll Timeout property
+        props.setProperty(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, "60000");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
+        kafkaConsumer.subscribe(List.of(topicName));
+
+        // main thread
+        Thread mainThread = Thread.currentThread();
+
+        // When Main() is closed, call Wakeup() Exception
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                logger.info("main program starts to exit by calling wakeup");
+                kafkaConsumer.wakeup();
+
+                try {
+                    // wait until main() is closed.
+                    mainThread.join();
+                } catch(InterruptedException e) { e.printStackTrace();}
+            }
+        });
+
+
+        // kafkaConsumer.close();
+        pollAutoCommit(kafkaConsumer);
+    }
+
+    // Make AutoCommit Method
+    public static void pollAutoCommit(KafkaConsumer<String, String> kafkaConsumer) {
+        int loopCnt = 0;
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
+                logger.info(" ######## loopCnt: {} consumerRecords count:{}", loopCnt++, consumerRecords.count());
+                for (ConsumerRecord record : consumerRecords) {
+                    logger.info("record key:{}, partition:{}, record offset:{}, record value:{}",
+                            record.key(), record.partition(), record.offset(), record.value());
+                }
+                try {
+                    logger.info("main thread is sleep {} ms during while loop", 10000);
+                    Thread.sleep(10000);
+                }catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }catch(WakeupException e) {
+            logger.error("wakeup exception has been called");
+        }finally {
+            logger.info("finally consumer is closing");
+            kafkaConsumer.close();
+        }
+    }
+}
