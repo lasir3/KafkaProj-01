1b08df8291f1a05e967f41782fdef415fd340726
edk2613 <lasir3@gmail.com>
2023-06-21 10:17:08 +0900

    add comment

--- seminar/src/main/java/com/seminar/kafka/producer/OrderSerdeProducer.java
+++ seminar/src/main/java/com/seminar/kafka/producer/OrderSerdeProducer.java
@@ -15,9 +15,11 @@ import java.util.Properties;
 
 public class OrderSerdeProducer {
     public static final Logger logger = LoggerFactory.getLogger(OrderSerdeProducer.class.getName());
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        // 토픽명 설정
         String topicName = "topic-to-goldi";
+        // Kafka Property 설정
         Properties props = new Properties();
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.73:9092, 192.168.0.73:9093");
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -28,24 +30,23 @@ public class OrderSerdeProducer {
         // props.setProperty("partitioner.class", "CustomPartitioner"); or
         props.setProperty(ProducerConfig.PARTITIONER_CLASS_CONFIG, "com.seminar.kafka.producer.CustomPartitioner");
 
-
-        // KafkaProducer Object Creation
+        // KafkaProducer 객체 생성
+        // Key : String, Value : OrderModel 객체
         KafkaProducer<String, OrderModel> kafkaProducer = new KafkaProducer<String, OrderModel>(props);
         // Async 방식으로 동작하도록 설정
-        boolean sync = false; 
+        boolean sync = false;
 
         // Producer 파일의 절대경로 지정
-        File file = new File( "E:\\Kafka-Project\\KafkaProj-01\\seminar\\src\\main\\resources\\pizza_append.txt");
+        File file = new File("E:\\Kafka-Project\\KafkaProj-01\\seminar\\src\\main\\resources\\pizza_append.txt");
         EventHandler eventHandler = new FileEventHandler(kafkaProducer, topicName, sync);
         FileEventSource fileEventSource = new FileEventSource(1000, file, eventHandler);
 
         // 파일의 변화를 감지하는 Event Thread 생성
         Thread fileEventSourceThread = new Thread(fileEventSource);
         fileEventSourceThread.start();
-
-
-
+        
         try {
+            //
             fileEventSourceThread.join();
         } catch (InterruptedException e) {
             logger.error(e.getMessage());
